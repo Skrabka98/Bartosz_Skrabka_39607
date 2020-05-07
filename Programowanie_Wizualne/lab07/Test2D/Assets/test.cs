eac25d1b7ae356d097ce8799a1e70ff4f1163904
Skrabka98 <barti.sk@gmail.com>
2020-05-07 17:56:35 +0200

    lab07 PW

---
+++ Programowanie_Wizualne/lab07/Test2D/Assets/test.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    public int x = 5;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < x; i++)
+        {
+            print("Bartosz");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
