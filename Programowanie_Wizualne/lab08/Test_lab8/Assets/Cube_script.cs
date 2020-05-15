df151386ff21b36a660ffac1248a98717a16012d
Skrabka98 <barti.sk@gmail.com>
2020-05-15 19:24:44 +0200

    wizualka_08

---
+++ Programowanie_Wizualne/lab08/Test_lab8/Assets/Cube_script.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Cube_script : MonoBehaviour
+{
+    Vector3 pozycja = new Vector3(10, 0, 0);
+
+    public float predkosc;
+    public String os;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //print(transform.position);
+        //print(transform.position.x);
+
+        //transform.position = pozycja;
+        //transform.position = new Vector3(5, 5, 5);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (os == "x")
+        {
+            transform.Rotate(predkosc, 0, 0);
+        }
+        if (os == "y")
+        {
+            transform.Rotate(0, predkosc, 0);
+        }
+        if (os == "z")
+        {
+            transform.Rotate(0, 0, predkosc);
+        }
+    }
+}
---
+++ Programowanie_Wizualne/lab08/Test_lab8/Assets/Script1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script1 : MonoBehaviour
+{
+    GameObject szukany;
+
+    GameObject[] szukane;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        szukany = GameObject.Find("Sphere");
+        //szukany = GameObject.FindGameObjectWithTag("do_usuniecia");
+
+        szukane = GameObject.FindGameObjectsWithTag("do_usuniecia");
+        foreach(GameObject s in szukane)
+        {
+            Destroy(s);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
