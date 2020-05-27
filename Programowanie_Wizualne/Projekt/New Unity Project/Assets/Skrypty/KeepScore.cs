d8875fde7051564311948b215870edb6386ed6ae
Skrabka98 <barti.sk@gmail.com>
2020-05-27 16:41:02 +0200

    add_coins

---
+++ Programowanie_Wizualne/Projekt/New Unity Project/Assets/Skrypty/KeepScore.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeepScore : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static int Score = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnGUI()
+    {
+        GUI.Box(new Rect(0, 0, 100, 100), "Zebrane\nPunkty\n\n" +Score);
+    }
+}
---
+++ Programowanie_Wizualne/Projekt/New Unity Project/Assets/Skrypty/Points.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Points : MonoBehaviour
+{
+    private float predkosc=1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, predkosc, 0);
+    }
+    void OnTriggerEnter(Collider collision)
+    {
+
+        if(collision.gameObject.tag == "Player")
+        {
+
+            KeepScore.Score += 1;
+            Destroy(transform.gameObject);
+        }
+    }
+}
